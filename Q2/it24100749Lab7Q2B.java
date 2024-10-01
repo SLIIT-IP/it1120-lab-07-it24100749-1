588536b313e35da5208036ad6614ded459fa5eed
it24100749 <it24100749@my.sliit.lk>
2024-10-01 20:51:22 +0530

    Add files via upload

---
+++ Q2/it24100749Lab7Q2B.java
@@ -0,0 +1,16 @@
+public class it24100749Lab7Q2B {
+
+    public static void main(String[] args) {
+               for (int i = 1; i <= 5; i++) {
+            
+            System.out.print(i + " - ");
+            
+                        for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            
+            
+            System.out.println();
+        }
+    }
+}
